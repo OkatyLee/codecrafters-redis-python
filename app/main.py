a56ee35bd1c1dfac48ab7fb22e86a199dee47565
OkatyLee <mvasilev801@gmail.com>
2026-02-26 22:57:44 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,14 +4,15 @@ import socket  # noqa: F401
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.listen()
+    server_socket.accept()
     while True:
-        client_socket, _ = server_socket.accept() # wait for client
-        data = client_socket.recv(1024)
-        print(f"Received data: {data}")
-        client_socket.send(b'+PONG\r\n')
+        client_socket, _ = server_socket.accept()
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            client_socket.sendall(b"+PONG\r\n")
         client_socket.close()
     
 
