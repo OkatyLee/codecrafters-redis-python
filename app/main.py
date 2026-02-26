b6ea65fc41d80cb741b085598dbada0f1bfd8fe6
OkatyLee <mvasilev801@gmail.com>
2026-02-26 22:28:03 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,14 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.listen()
+    while True:
+        client_socket, _ = server_socket.accept() # wait for client
+        data = client_socket.recv(1024)
+        print(f"Received data: {data}")
+        client_socket.send(b'+PONG\r\n')
+        client_socket.close()
+    
 
 
 if __name__ == "__main__":
